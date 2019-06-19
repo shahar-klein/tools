19c0476dcf15af32f14f98a5352b4e679fffc7d9
root <shaharklein@gmail.com>
2019-06-19 23:55:49 +0000

    Add OVS fwd and nat

--- rp_test.bash
+++ rp_test.bash
@@ -1,9 +1,10 @@
+wait
 #!/bin/bash
 
 # TODO
 # log() taking a param as a file
 # Bandwidth stats in the RP instead of ssh
-
+# script for setting up OVS NAT rules instead of sshing 
 # 
 
 #
@@ -55,7 +56,6 @@
 #[3 - 5 : without offloads]
 
 
-
 PT_PCI_DEVICE1_BUS=0xb4
 PT_PCI_DEVICE1_SLOT=0x00
 PT_PCI_DEVICE1_FUNCTION=0x0
@@ -74,7 +74,7 @@ VF_PCI_DEVICE2_FUNCTION=0x2
 
 
 PRIV_NET="5.5.5.0/24"
-PUB_NET="20.20.20.0/24"
+PUB_NET="30.30.30.0/24"
 
 GFN_PUB_PORT_START=8000
 GS_PORT_START=10000
@@ -87,7 +87,7 @@ LOADER_IP=5.5.5.5
 
 INITIATOR=10.0.0.148
 INITIATOR_DEV=ens2
-INITIATOR_IP=20.20.20.20
+INITIATOR_IP=30.30.30.20
 
 RPVM_PT=rp
 RPVM_SROV=rp_vf
@@ -97,17 +97,21 @@ RP=192.168.122.5
 RP_PRIV_LEG_IP=5.5.5.1
 RP_PRIV_LEG_DEV=enp4s0
 
-NUM_SESSIONS=500
+NUM_SESSIONS=5
 BW_PER_SESSION=20m
 
-RP_PUB_LEG_IP=20.20.20.100
+RP_PUB_LEG_IP=30.30.30.100
 RP_PUB_LEG_DEV=enp7s0
 
+BRPUB=brpub
+BRPRIV=brpriv
+RP_PRIV_PATCH_PORT=priv-patch
+RP_PUB_PATCH_PORT=pub-patch
 
 TEST=${1:?test name not set}
 
 # in seconds
-DURATION=20
+DURATION=300
 LOG_DURATION=$((DURATION-5))
 NUM_CPUS=8
 # in seconds
@@ -141,7 +145,7 @@ P2KILL=""
 TEST_PROFILE="IP_FORWARDING_MULTI_STREAM_0_LOSS"
 
 #TESTS="linux_fwd linux_fwd_nat ovs_fwd ovs_fwd_nat ovs_fwd_ct"
-TESTS="linux_fwd"
+TESTS="ovs_fwd_nat"
 
 #NIC_MODES="pt sriov"
 NIC_MODES="pt"
@@ -151,6 +155,13 @@ CPU_BINDINGS="dangling"
 
 #CPU_AFFINITIES="4 8"
 CPU_AFFINITIES="4"
+
+LOADER_CMD=
+INITIATOR_CMD=
+LOADER_DEV_MAC=
+INITIATOR_DEV_MAC=
+RP_PRIV_LEG_MAC=
+RP_PUB_LEG_MAC=
 
 log() {
 	d=`date +[%d:%m:%y" "%H:%M:%S:%N]`
@@ -229,20 +240,24 @@ flush_ip_dev() {
 }
 
 shutdown_vm() {
-	VM=$1
+	log "shutting down $RPVM"
 
-	log "shutting down $VM"
-
-	virsh shutdown $VM
+	set +e
+	virsh shutdown $RPVM
+	set -e
+	sleep 2
 }
 
 startup_vm() {
-	VM=$1
-	VMIP=$2
+	VM=$RPVM
+	VMIP=$RP
 
 	log "starting $VM"
 
+	set +e
 	virsh start $VM
+	set -e
+
 	sleep 2
 	local chars=( \| / – \\ )
 	local i=0
@@ -255,6 +270,7 @@ startup_vm() {
                 	sleep 0.3
 
 	done
+	sleep 2
 }
 
 reboot_vm() {
@@ -293,6 +309,13 @@ setup() {
 }
 
 setup_vm() {
+
+	# https://www.kernel.org/doc/Documentation/networking/ip-sysctl.txt:re arp
+ 	logCMD "ssh $RP sysctl net.ipv4.conf.all.arp_ignore=2"
+ 	logCMD "ssh $RP sysctl net.ipv4.conf.all.arp_announce=1"
+
+	RP=`virsh domifaddr $RPVM | grep ipv4 | awk '{print $4}'| cut -d"/" -f1`
+
 	RP_PRIV_LEG_MAC=`get_mac_dev $RP $RP_PRIV_LEG_DEV`
 	flush_ip_dev $RP $RP_PRIV_LEG_DEV
 	set_ip_dev $RP $RP_PRIV_LEG_DEV $RP_PRIV_LEG_IP
@@ -304,6 +327,71 @@ setup_vm() {
 	RP_PUB_LEG_MAC=`get_mac_dev $RP $RP_PUB_LEG_DEV`
 	flush_ip_dev $RP $RP_PUB_LEG_DEV
 	set_ip_dev $RP $RP_PUB_LEG_DEV $RP_PUB_LEG_IP
+
+	logCMD "ssh $RP ethtool -X $RP_PRIV_LEG_DEV hfunc toeplitz"
+	logCMD "ssh $RP ethtool -X $RP_PUB_LEG_DEV hfunc toeplitz"
+}
+
+setup_vm_ovs() {
+
+	offload=$1
+	logCMD "ssh $RP ovs-vsctl set open . other-config:hw-offload=false"
+	if [ $offload = "yes" ]
+	then
+		logCMD "ssh $RP ovs-vsctl set open . other-config:hw-offload=true"
+		logCMD "ssh $RP ethtool -K $RP_PRIV_LEG_DEV hw-tc-offload on"
+		logCMD "ssh $RP ethtool -K $RP_PUB_LEG_DEV hw-tc-offload on"
+	fi
+	logCMD "ssh $RP systemctl restart openvswitch-switch.service"
+
+	logCMD "ssh $RP ovs-vsctl add-br $BRPRIV"
+	logCMD "ssh $RP ovs-ofctl del-flows $BRPRIV"
+	logCMD "ssh $RP ovs-vsctl add-port $BRPRIV $RP_PRIV_LEG_DEV"
+
+	logCMD "ssh $RP ovs-vsctl add-br $BRPUB"
+	logCMD "ssh $RP ovs-ofctl del-flows $BRPUB"
+	logCMD "ssh $RP ovs-vsctl add-port $BRPUB $RP_PUB_LEG_DEV"
+
+	flush_ip_dev $RP $RP_PRIV_LEG_DEV
+	flush_ip_dev $RP $RP_PUB_LEG_DEV
+
+	set_ip_dev $RP $BRPRIV $RP_PRIV_LEG_IP
+	set_ip_dev $RP $BRPUB $RP_PUB_LEG_IP
+
+	logCMD "ssh $RP ip link set dev $RP_PRIV_LEG_DEV up"
+	logCMD "ssh $RP ip link set dev $RP_PUB_LEG_DEV up"
+	logCMD "ssh $RP ip link set dev $BRPRIV up"
+	logCMD "ssh $RP ip link set dev $BRPUB up"
+
+	# For VFs we need to update the bridge MAC addresses
+	if [ $mode = "sriov" ]
+	then
+		RP_PRIV_LEG_MAC=`get_mac_dev $RP $RP_PRIV_LEG_DEV`
+		RP_PUB_LEG_MAC=`get_mac_dev $RP $RP_PUB_LEG_DEV`
+
+		logCMD "ssh $RP ovs-vsctl set bridge $BRPRIV other-config:hwaddr=\"$RP_PRIV_LEG_MAC\""
+		logCMD "ssh $RP ovs-vsctl set bridge $BRPUB other-config:hwaddr=\"$RP_PUB_LEG_MAC\""
+
+	fi
+
+	# Create patch ports
+	logCMD "ssh $RP ovs-vsctl add-port $BRPRIV $RP_PRIV_PATCH_PORT -- set interface $RP_PRIV_PATCH_PORT type=patch options:peer=$RP_PUB_PATCH_PORT"
+	logCMD "ssh $RP ovs-vsctl add-port $BRPUB $RP_PUB_PATCH_PORT -- set interface $RP_PUB_PATCH_PORT type=patch options:peer=$RP_PRIV_PATCH_PORT"
+
+	# Add ARP to the priv bridge
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPRIV priority=10,in_port=$RP_PRIV_LEG_DEV,arp,action=normal"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPRIV priority=10,in_port=$BRPRIV,arp,action=normal"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPRIV priority=50,in_port=$RP_PRIV_PATCH_PORT,arp,action=drop"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPRIV priority=50,in_port=$RP_PRIV_PATCH_PORT,ip6,action=drop"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPRIV priority=50,in_port=$RP_PRIV_PATCH_PORT,dl_dst=ff:ff:ff:ff:ff:ff,action=drop"
+	
+	
+	# Add ARP to the pub bridge
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPUB priority=10,in_port=$RP_PUB_LEG_DEV,arp,action=normal"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPUB priority=10,in_port=$BRPUB,arp,action=normal"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPUB priority=50,in_port=$RP_PUB_PATCH_PORT,arp,action=drop"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPUB priority=50,in_port=$RP_PUB_PATCH_PORT,ip6,action=drop"
+	logCMD "ssh $RP ovs-ofctl add-flow $BRPUB priority=50,in_port=$RP_PUB_PATCH_PORT,dl_dst=ff:ff:ff:ff:ff:ff,action=drop"
 }
 
 
@@ -375,6 +463,7 @@ log_before() {
 	
 }
 
+# XXX Might want to keep the config around if we want to debug
 cleanup() {
 	#clean all potential datapaths
 	#fwd
@@ -393,7 +482,6 @@ cleanup() {
 	ssh $LOADER pkill gonoodle
 	ssh $INITIATOR pkill gonoodle
 
-	pkill mpstat
 	set -e
 }
 
@@ -497,17 +585,17 @@ plotLogs() {
 runTest() {
 	#start loader
 	echo "staring loaded..."
-	cmdBG "ssh $LOADER /root/ws/git/gonoodle/gonoodle -u -c $INITIATOR_IP --rp loader -C $NUM_SESSIONS -R $NUM_SESSIONS -M 10 -b $BW_PER_SESSION -p ${GS_PORT_START} -L :${RP_PORT_START} -l 1000 -t $DURATION"
+	cmdBG $LOADER_CMD
 	sleep 1
 
 
 	echo "staring initiator..."
-	cmdBG "ssh $INITIATOR /root/ws/git/gonoodle/gonoodle -u -c $LOADER_IP --rp initiator -C $NUM_SESSIONS -R $NUM_SESSIONS -M 1 -b 1k -p ${GFN_PUB_PORT_START} -L :${RP_PORT_START} -l 1000 -t $DURATION"
+	cmdBG $INITIATOR_CMD
 
 }
 
 runMetrics() {
-	collectBWLogs &
+	# collectBWLogs &
 	P2KILL+="$! "
 	for ((cpus=0;cpus<NUM_CPUS;cpus++))
 	do
@@ -516,6 +604,8 @@ runMetrics() {
 		P2KILL+="$! "
 
 	done
+	echo "Waiting.."
+	wait
 }
 
 host_vm_cpu_binding() {
@@ -544,32 +634,80 @@ rp_irq_affinity() {
 
 linux_forward_setup() {
 	ssh $RP sysctl net.ipv4.ip_forward=1
+	LOADER_CMD="ssh $LOADER /root/ws/git/gonoodle/gonoodle -u -c $INITIATOR_IP --rp loader -C $NUM_SESSIONS -R $NUM_SESSIONS -M 10 -b $BW_PER_SESSION -p ${GFN_PUB_PORT_START} -L :${GS_PORT_START} -l 1000 -t $DURATION"
+	INITIATOR_CMD="ssh $INITIATOR /root/ws/git/gonoodle/gonoodle -u -c $LOADER_IP --rp initiator -C $NUM_SESSIONS -R $NUM_SESSIONS -M 1 -b 1k -p ${GS_PORT_START} -L :${GFN_PUB_PORT_START} -l 1000 -t $DURATION"
 }
 
+#GFN_PUB_PORT_START=8000
+#GS_PORT_START=10000
+#RP_PORT_START=5000
+
 linux_forward_nat_setup() {
-	ssh $RP for i in {0..1000} ; do let dp=${GFN_PUB_PORT_START}+$i; let tdp=${GS_PORT_START}+$i ; iptables -t nat -A PREROUTING -i ens6 -p udp -m udp --dport $dp -j DNAT --to-destination ${LOADER_IP}:$tdp ; done
+	nat_cmd="for i in {0..1000} ; do let dp=$GFN_PUB_PORT_START+\$i; let tdp=$GS_PORT_START+\$i ; iptables -t nat -A PREROUTING -i $RP_PUB_LEG_DEV -p udp -m udp --dport \$dp -j DNAT --to-destination ${LOADER_IP}:\$tdp ; done"
+	ssh $RP $nat_cmd
 	ssh $RP iptables -t nat -A POSTROUTING -o ${RP_PUB_LEG_DEV} -j SNAT --to-source ${RP_PUB_LEG_IP}
 	ssh $RP iptables -t nat -A POSTROUTING -o ${RP_PRIV_LEG_DEV} -j SNAT --to-source ${RP_PRIV_LEG_IP}
+	LOADER_CMD="ssh $LOADER /root/ws/git/gonoodle/gonoodle -u -c $RP_PRIV_LEG_IP --rp loader -C $NUM_SESSIONS -R $NUM_SESSIONS -M 10 -b $BW_PER_SESSION -p ${RP_PORT_START} -L :${GS_PORT_START} -l 1000 -t $DURATION"
+	INITIATOR_CMD="ssh $INITIATOR /root/ws/git/gonoodle/gonoodle -u -c $RP_PUB_LEG_IP --rp initiator -C $NUM_SESSIONS -R $NUM_SESSIONS -M 1 -b 1k -p ${GFN_PUB_PORT_START} -L :${RP_PORT_START} -l 1000 -t $DURATION"
 }
 
 ovs_forward_setup() {
+	setup_vm_ovs "no"
+
+	# Add forwarding rules
+	ssh $RP ovs-ofctl add-flow $BRPUB priority=100,in_port=$RP_PUB_LEG_DEV,udp,nw_dst=$LOADER_IP,action=$RP_PUB_PATCH_PORT
+	ssh $RP ovs-ofctl add-flow $BRPRIV priority=100,in_port=$RP_PRIV_PATCH_PORT,udp,nw_dst=$LOADER_IP,action=mod_dl_src=$RP_PRIV_LEG_MAC,mod_dl_dst=$LOADER_DEV_MAC,$RP_PRIV_LEG_DEV
+	ssh $RP ovs-ofctl add-flow $BRPRIV priority=100,in_port=$RP_PRIV_LEG_DEV,udp,nw_dst=$INITIATOR_IP,action=$RP_PRIV_PATCH_PORT
+	ssh $RP ovs-ofctl add-flow $BRPUB priority=100,in_port=$RP_PUB_PATCH_PORT,udp,nw_dst=$INITIATOR_IP,action=mod_dl_src=$RP_PUB_LEG_MAC,mod_dl_dst=$INITIATOR_DEV_MAC,$RP_PUB_LEG_DEV
+	LOADER_CMD="ssh $LOADER /root/ws/git/gonoodle/gonoodle -u -c $INITIATOR_IP --rp loader -C $NUM_SESSIONS -R $NUM_SESSIONS -M 10 -b $BW_PER_SESSION -p ${GFN_PUB_PORT_START} -L :${GS_PORT_START} -l 1000 -t $DURATION"
+	INITIATOR_CMD="ssh $INITIATOR /root/ws/git/gonoodle/gonoodle -u -c $LOADER_IP --rp initiator -C $NUM_SESSIONS -R $NUM_SESSIONS -M 1 -b 1k -p ${GS_PORT_START} -L :${GFN_PUB_PORT_START} -l 1000 -t $DURATION"
 }
 
+ovs_forward_nat_setup() {
+	setup_vm_ovs "no"
+	echo "Done setting up OVS"
+
+	# XXX It'll take a long time to add these flows via ssh!
+	for ((i = 0; i < $NUM_SESSIONS; i++)); do
+		GC_PORT=$((GFN_PUB_PORT_START+i))
+		RP_PORT=$((RP_PORT_START+i))
+		GS_PORT=$((GS_PORT_START+i))
+
+		# Add the pub side of the flows
+		ssh $RP ovs-ofctl add-flow $BRPUB priority=100,in_port=$RP_PUB_LEG_DEV,udp,nw_dst=$RP_PUB_LEG_IP,tp_dst=$RP_PORT,action=mod_nw_dst=$LOADER_IP,mod_tp_dst=$GS_PORT,$RP_PUB_PATCH_PORT
+		ssh $RP ovs-ofctl add-flow $BRPRIV priority=100,in_port=$RP_PRIV_PATCH_PORT,udp,nw_dst=$LOADER_IP,tp_dst=$GS_PORT,action=mod_nw_src=$RP_PRIV_LEG_IP,mod_tp_src=$RP_PORT,mod_dl_src=$RP_PRIV_LEG_MAC,mod_dl_dst=$LOADER_DEV_MAC,$RP_PRIV_LEG_DEV
+
+		# Add the priv _side of the flows
+		ssh $RP ovs-ofctl add-flow $BRPRIV priority=100,in_port=$RP_PRIV_LEG_DEV,udp,nw_dst=$RP_PRIV_LEG_IP,tp_dst=$RP_PORT,action=mod_nw_dst=$INITIATOR_IP,mod_tp_dst=$GC_PORT,$RP_PRIV_PATCH_PORT
+		ssh $RP ovs-ofctl add-flow $BRPUB priority=100,in_port=$RP_PUB_PATCH_PORT,udp,nw_dst=$INITIATOR_IP,tp_dst=$GC_PORT,action=mod_nw_src=$RP_PUB_LEG_IP,mod_tp_src=$RP_PORT,mod_dl_src=$RP_PUB_LEG_MAC,mod_dl_dst=$INITIATOR_DEV_MAC,$RP_PUB_LEG_DEV
+	done
+
+	LOADER_CMD="ssh $LOADER /root/ws/git/gonoodle/gonoodle -u -c $RP_PRIV_LEG_IP --rp loader -C $NUM_SESSIONS -R $NUM_SESSIONS -M 10 -b $BW_PER_SESSION -p ${RP_PORT_START} -L :${GS_PORT_START} -l 1000 -t $DURATION"
+	INITIATOR_CMD="ssh $INITIATOR /root/ws/git/gonoodle/gonoodle -u -c $RP_PUB_LEG_IP --rp initiator -C $NUM_SESSIONS -R $NUM_SESSIONS -M 1 -b 1k -p ${RP_PORT_START} -L :${GFN_PUB_PORT_START} -l 1000 -t $DURATION"
+
+}
+
+
+
 # linux_fwd linux_fwd_nat ovs_fwd ovs_fwd_nat ovs_fwd_ct
+# XXX non-offload case
 setup_tests() {
 	tests=$1
 	case $tests in 
 		linux_fwd)
-			linux_forward_setup()
+			linux_forward_setup
 			;;
 		linux_fwd_nat)
-			linux_forward_nat_setup()
+			linux_forward_nat_setup
 			;;
-		#ovs_fwd)
-		#	ovs_forward_setup()
-		#	;;
+		ovs_fwd)
+			ovs_forward_setup
+			;;
+		ovs_fwd_nat)
+			ovs_forward_nat_setup
+			;;
 		#ovs_fwd_ct)
-		#	ovs_forward_nat_setup()
+		#	ovs_forward_ct_setup
 		#	;;
 	esac
 }
@@ -594,54 +732,52 @@ cleanup
 
 echo "Init test"
 initTest
-echo "Run test"
 
-set +e
-shutdown_vm
-set -e
+# XXX Add a loop for number of sessions : 500, 1000
+# XXX Burst too.
+# XXX H/A
 for mode in $NIC_MODES
 do
-	log_before
-	if [ $mode -eq "pt" ] ; then
+	if [ $mode = "pt" ] ; then
 		RPVM=rp
 	else
 		RPVM=rp_vf
 	fi
-	startup_vm
-	RP=`virsh domifaddr $RPVM | grep ipv4 | awk '{print $4}'| cut -d"/" -f1`
+	shutdown_vm
 	for profile in $TEST_PROFILE
 	do
 		startup_vm
+		log_before
 		setup_vm
-		ssh $RP mlnx_tune -p $profile
+		ssh $RP mlnx_tune -p $profile > $LOGDIR/mlnx_tune.log
 		for cpu_binding in $CPU_BINDINGS
 		do
-			host_vm_cpu_binding($cpu_binding)
+			host_vm_cpu_binding $cpu_binding
 			for  cpu_affinity in $CPU_AFFINITIES
 			do
-				rp_irq_affinity($cpu_affinity)
+				rp_irq_affinity $cpu_affinity
+				# echo "$profile, $cpu_binding, $cpu_affinity, $mode, $test"
+				for t in $TESTS
 				do
-					# echo "$profile, $cpu_binding, $cpu_affinity, $mode, $test"
-					for t in $TESTS
-					do
-						setup_test $t
-						run_Test
-						runMetrics
-						cleanup
-						TEST_LOG_DIR="${mode}_${profile}_${cpu_binding}_${cpu_affinity}_${t}"
-						mkdir -p /tmp/${TEST_LOG_DIR}
-						mv $LOGDIR/* /tmp/${TEST_LOG_DIR}
-						mv /tmp/${TEST_LOG_DIR} $LOGDIR
-					done
+					setup_tests $t
+					echo "Run test"
+					runTest
+					echo "Done Running test"
+					runMetrics
+					echo "Done Collecting runMetrics"
+					cleanup
+					TEST_LOG_DIR="${mode}_${profile}_${cpu_binding}_${cpu_affinity}_${t}"
+					mkdir -p /tmp/${TEST_LOG_DIR}
+					mv $LOGDIR/* /tmp/${TEST_LOG_DIR}
+					mv /tmp/${TEST_LOG_DIR} $LOGDIR
 				done
 			done
 		done
 		shutdown_vm
 	done
-	log_after
+	# log_after
 	shutdown_vm
 done
-
 
 #log_before
 
