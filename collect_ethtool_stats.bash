8e1077f4ce75b8dc7e5214aeca600ab2031bc98f
root <shaharklein@gmail.com>
2019-06-25 12:42:38 -0700

    set_ovs_config missing a few parms

--- collect_ethtool_stats.bash
+++ collect_ethtool_stats.bash
@@ -15,7 +15,7 @@ rm -f $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.dropped
 
 BASE_RX_BYTES=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_bytes_phy:" | awk '{print  $2}'`
 BASE_TX_BYTES=`ethtool -S $RP_PUB_LEG_DEV | grep "tx_bytes_phy:" | awk '{print  $2}'`
-BASE_RX_DROPPED=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_buffer:" | awk '{print  $2}'`
+BASE_RX_DROPPED=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_of_buffer:" | awk '{print  $2}'`
 BASE_TX_DROPPED=`ethtool -S $RP_PUB_LEG_DEV | grep "tx_queue_dropped:" | awk '{print  $2}'`
 for (( dur=1; dur<=$DURATION; dur++ ))
 do
@@ -30,11 +30,11 @@ do
         echo $dur $((tx_bytes-BASE_TX_BYTES)) >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.tput
 
         # RX buffer overruns
-        rx_dropped=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_buffer:" | awk '{print  $2}'`
+        rx_dropped=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_of_buffer:" | awk '{print  $2}'`
         echo $dur $((rx_dropped-BASE_RX_DROPPED)) >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.dropped
 
         # TX drops
-        tx_dropped=`ethtool -S $RP_PUB_LEG_DEV | grep "rx_out_buffer:" | awk '{print  $2}'`
+        tx_dropped=`ethtool -S $RP_PUB_LEG_DEV | grep "tx_queue_dropped:" | awk '{print  $2}'`
         echo $dur $((tx_dropped-BASE_TX_DROPPED)) >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.dropped
 
         BASE_RX_BYTES=$rx_bytes
