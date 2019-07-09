8b6140f67a82db06270f454546eca1a19c8b1d88
root <shaharklein@gmail.com>
2019-07-09 05:05:05 -0700

    add red for low BW

--- rp-test/quick-scan-results.bash
+++ rp-test/quick-scan-results.bash
@@ -17,7 +17,7 @@ for DIR in `ls -tr -d $SCANDIR/*/` ; do
 	for DEV in $DEVS ; do
 		DEVP=`echo $DEV| cut -f1 -d.`
 		echo -n $DEVP":"
-		cat $DIR/$DEV | awk '{sum+=$2} END {printf(" %.2f GBit/s. ", sum*8/(NR*1000000000))}' 
+		cat $DIR/$DEV | awk '{sum+=$2} END {{BW=sum*8/(NR*1000000000)} if (BW < 1) {printf("\033[31m") }{printf(" %.2f GBit/s. ", BW)} {printf("\033[37m")}}' 
 	done
 	cat $DIR/*.idle |  awk '{sum+=$2} END {printf("Total CPU Usage: %.2f%. ", 100-sum/NR)}'
 	cat $DIR/*.guest |  awk '{sum+=$2} END {printf("Guest CPU Usage: %.2f%. ", sum/NR)}'
