a0ea43b3d182e37454638860fa6ffe20b98bc392
root <shaharklein@gmail.com>
2019-07-17 08:56:51 -0700

    Stats for CPU and bandwidth/drop is changed
    CPU is now in <cpu#>.info and the format is "sys guest idle"
    bandwidth and drop are now the cumulative number, not the incremental

--- rp-test/collect_ethtool_stats.bash
+++ rp-test/collect_ethtool_stats.bash
@@ -28,28 +28,38 @@ BASE_TX_BYTES=`ethtool -S $RP_PUB_LEG_DEV | grep $txbytes | awk '{print  $2}'`
 BASE_RX_DROPPED=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_of_buffer:" | awk '{print  $2}'`
 BASE_TX_DROPPED=`ethtool -S $RP_PUB_LEG_DEV | grep "tx_queue_dropped:" | awk '{print  $2}'`
 
+echo $BASE_RX_BYTES  > $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.tput
+echo $BASE_TX_BYTES > $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.tput
+echo $BASE_RX_DROPPED > $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.dropped
+echo $BASE_TX_DROPPED > $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.dropped
+
 for (( dur=1; dur<=$DURATION; dur++ ))
 do
 
         sleep $sleep_duration
         # RX bytes
         rx_bytes=`ethtool -S $RP_PRIV_LEG_DEV | grep $rxbytes | awk '{print  $2}'`
-        echo $dur $((rx_bytes-BASE_RX_BYTES)) >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.tput
+        tx_bytes=`ethtool -S $RP_PUB_LEG_DEV | grep $txbytes | awk '{print  $2}'`
+        rx_dropped=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_of_buffer:" | awk '{print  $2}'`
+        tx_dropped=`ethtool -S $RP_PUB_LEG_DEV | grep "tx_queue_dropped:" | awk '{print  $2}'`
+
+        #echo $dur $((rx_bytes-BASE_RX_BYTES)) >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.tput
+        echo $dur $rx_bytes >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.tput
 
         # TX bytes
-        tx_bytes=`ethtool -S $RP_PUB_LEG_DEV | grep $txbytes | awk '{print  $2}'`
-        echo $dur $((tx_bytes-BASE_TX_BYTES)) >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.tput
+        #echo $dur $((tx_bytes-BASE_TX_BYTES)) >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.tput
+        echo $dur $tx_bytes >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.tput
 
         # RX buffer overruns
-        rx_dropped=`ethtool -S $RP_PRIV_LEG_DEV | grep "rx_out_of_buffer:" | awk '{print  $2}'`
-        echo $dur $((rx_dropped-BASE_RX_DROPPED)) >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.dropped
+        #echo $dur $((rx_dropped-BASE_RX_DROPPED)) >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.dropped
+        echo $dur $rx_dropped >> $RESULTS_LOG_DIR/${RP_PRIV_LEG_DEV}.dropped
 
         # TX drops
-        tx_dropped=`ethtool -S $RP_PUB_LEG_DEV | grep "tx_queue_dropped:" | awk '{print  $2}'`
-        echo $dur $((tx_dropped-BASE_TX_DROPPED)) >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.dropped
+        #echo $dur $((tx_dropped-BASE_TX_DROPPED)) >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.dropped
+        echo $dur $tx_dropped >> $RESULTS_LOG_DIR/${RP_PUB_LEG_DEV}.dropped
 
-        BASE_RX_BYTES=$rx_bytes
-        BASE_TX_BYTES=$tx_bytes
-        BASE_RX_DROPPED=$rx_dropped
-        BASE_TX_DROPPED=$tx_dropped
+        #BASE_RX_BYTES=$rx_bytes
+        #BASE_TX_BYTES=$tx_bytes
+        #BASE_RX_DROPPED=$rx_dropped
+        #BASE_TX_DROPPED=$tx_dropped
 done
