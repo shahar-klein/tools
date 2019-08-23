8db8756d5326618bb2f10eeaf6f137c08ccee966
Shahar Klein <sklein@nvidia.com>
2019-08-22 19:40:01 -0700

    Add script to monitor BW by L4 port

---
+++ L4portBW.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -u
+DEV=${1:?Missing DEV}
+PORT=${2:?Missing port}
+PCAP=/tmp/2.pcap
+
+clear
+echo
+echo
+echo
+
+
+while [ 1 ] ; do
+        timeout 1.5 tcpdump -ni $DEV  -q udp port $PORT --direction=out -w $PCAP >/dev/null 2>&1
+        L=`tcpdump -nr $PCAP -ttttt --time-stamp-precision=micro -K -s 0 --number -q 2>&1 | grep -v reading | grep 00:00:00 | tail -n1 | awk '{print $1}'`
+        cmd="sed -n '1,${L}p'"
+        MB=`tcpdump -nr $PCAP -ttttt --time-stamp-precision=micro -K -s 0 --number -q 2>&1 | grep -v reading | grep 00:00:00  | sed -n "1,${L}p" | grep length | awk 'BEGIN {sum=0} {sum+=$9} END {print sum*8/1000000}'`
+        echo $DEV port $PORT: $MB
+        echo -e "\033[2A"
+        sleep 0.3
+done
