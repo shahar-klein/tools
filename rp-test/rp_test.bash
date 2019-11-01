0dd0fe68a74e61da6f798b392382f6eca7dac261
root <shaharklein@gmail.com>
2019-11-01 14:59:38 -0700

    restore updating tools on VM.

--- rp-test/rp_test.bash
+++ rp-test/rp_test.bash
@@ -422,7 +422,7 @@ setup_vm() {
 
 
 	set +e
-	# ssh $RP "cd $TOOLS ; git reset --hard ; git pull --force --no-edit" 2>&1 > /dev/null
+	ssh $RP "cd $TOOLS ; git reset --hard ; git pull --force --no-edit" 2>&1 > /dev/null
 	if [ $MULTI_IP = yes ] ; then
 		logCMD "ssh $RP ip route add $PUB_NET via $RP_PUB_LEG_IP dev $RP_PUB_LEG_DEV 2>&1 > /dev/null"
 		logCMD "ssh $RP ip route add $PRIV_NET via $RP_PRIV_LEG_IP dev $RP_PRIV_LEG_DEV 2>&1 > /dev/null"
