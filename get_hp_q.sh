7c27616ac52c83098354b5c5d642e54a4342a963
root <shaharklein@gmail.com>
2020-09-24 10:25:59 -0700

    Add non PP stats

--- get_hp_q.sh
+++ get_hp_q.sh
@@ -47,6 +47,21 @@ if [ -z $dev ] ; then
 	Usage 2
 fi
 
+tc -s filter show dev $dev root | grep -q priority
+if [ $? != 0 ] ; then
+	if [[ $ONLY_HW = no ]] ; then
+		tc -s filter show dev $dev root | grep -A5 mirred | egrep "software|hardware" | tr '\r\n' ' ' | awk '{printf "hp_hw_q0_bytes: %.0f\nhp_hw_q0_pkts:  %.0f\nhp_sw_q0_bytes: %.0f\nhp_sw_q0_pkts:  %.0f\n", $9, $11, $3, $5}'
+	fi
+
+	if [[ $ONLY_HW = yes ]] ; then
+		tc -s filter show dev $dev root | grep -A5 mirred | egrep "software|hardware" | tr '\r\n' ' ' | awk '{printf "hp_hw_q0_bytes: %.0f\nhp_hw_q0_pkts:  %.0f\n", $9, $11}'
+	fi
+	exit 0
+fi
+
+
+
+
 #port | queue(hex) | sw bytes | sw pkts | hw bytes | hw pkts
 tc -s filter show dev $dev root | egrep -A 6 'filter protocol ip.*handle|priority|src_port|mirred' | egrep 'filter protocol ip.*handle|Sent software|Sent hardware|priority|src_port' | sed 's|bytes||; s|pkt||; s|src_port||; /filter protocol/c\aaa' | tr -s ' ' | sed 's|none|:0| ; s|action order 1: skbedit priority :|| ; s|pipe||; s|Sent software||; s|Sent hardware||' | tr '\r\n' ' ' | sed 's|aaa|\n|g' | tr -s  ' ' > /tmp/CL
 
@@ -56,12 +71,12 @@ if [[ $map = yes ]] ; then
 fi
 
 if [[ $ONLY_HW = yes ]] ; then
-	awk 'NF {printf "hp_hw_q%d_bytes: %.0f\nhp_hw_q%d_pkts: %.0f\n", "0x" $2, $5, "0x" $2, $6}' /tmp/CL
+	awk 'NF {printf "hp_hw_q%d_bytes: %.0f\nhp_hw_q%d_pkts:  %.0f\n", "0x" $2, $5, "0x" $2, $6}' /tmp/CL
 	exit 0
 fi
 
 if [[ $ONLY_HW = no ]] ; then
-	awk 'NF {printf "hp_hw_q%d_bytes: %.0f\nhp_hw_q%d_pkts: %.0f\nhp_sw_q%d_bytes: %.0f\nhp_sw_q%d_pkts: %.0f\n", "0x" $2, $5, "0x" $2, $6, "0x" $2, $3, "0x" $2, $4}' /tmp/CL
+	awk 'NF {printf "hp_hw_q%d_bytes: %.0f\nhp_hw_q%d_pkts:  %.0f\nhp_sw_q%d_bytes: %.0f\nhp_sw_q%d_pkts:  %.0f\n", "0x" $2, $5, "0x" $2, $6, "0x" $2, $3, "0x" $2, $4}' /tmp/CL
 	exit 0
 fi
 
