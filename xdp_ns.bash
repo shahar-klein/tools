9b06e3b7cfedd07029aebd30cd44bb1ec380928e
root <shaharklein@gmail.com>
2021-10-19 16:33:30 +0000

    WIP

--- xdp_ns.bash
+++ xdp_ns.bash
@@ -53,8 +53,10 @@ VLAN=${6:?Need vlan}
 
 OF_VLAN=$(( 4096 + VLAN ))
 
-XDP_OVSBR=brp1
+XDP_OVSBR=ovsbr1
 
+# Take uplink from bridge
+XDP_UPLINK=p0
 
 # Create a ns using some tenant name
 NS="${TENANT}-${VLAN}-tcpxdp-ns"
@@ -72,7 +74,8 @@ ip link set $VETH_OVS_SIDE up
 ip link set $VETH_XDP_SIDE up
 ip link set $SF up
 ip link set $SF_REP up
-MAC_SF=`cat /sys/class/net/${VETH_OVS_SIDE}/address`
+#MAC_SF=`cat /sys/class/net/${VETH_OVS_SIDE}/address`
+MAC_SF=02:e9:b1:75:d0:ac
 MAC_VETH_NS=`cat /sys/class/net/${VETH_XDP_SIDE}/address`
 # Give one end of veth to ns
 ip link set $VETH_XDP_SIDE netns $NS
@@ -101,7 +104,7 @@ ip netns exec $NS ip route add default via $GW dev $SF
 # ip ro add 'subnet' via veth
 ip netns exec $NS ip route add $SUBNET dev $VETH_XDP_SIDE
 
-BR_INT_PATCH=2
+BR_INT_PATCH=11
 
 # Give the other end to ovs
 ovs-vsctl --may-exist add-port $XDP_OVSBR $VETH_OVS_SIDE
@@ -109,10 +112,16 @@ ovs-vsctl --may-exist add-port $XDP_OVSBR $SF_REP
 
 ## on $XDP_OVSBR, add the flows ##A
 
-ovs-ofctl del-flows brp1 table=0,in_port=$VETH_OVS_SIDE
+ovs-ofctl del-flows ${XDP_OVSBR} table=0,in_port=$VETH_OVS_SIDE
+
+# Send ARP coming in from the wire to the ns
+ovs-ofctl add-flow $XDP_OVSBR  table=0,dl_vlan=$VLAN,arp,in_port=${XDP_UPLINK},actions=normal,strip_vlan,output:$SF_REP
+
+# Send ARP coming in from the patch port to the ns
+ovs-ofctl add-flow $XDP_OVSBR table=0,dl_vlan=$VLAN,arp,in_port=$BR_INT_PATCH,actions=normal,strip_vlan,output:$VETH_OVS_SIDE
 
 #/* Send the packet coming into the RP’s public to the tenant / vlan specific NS */
-ovs-ofctl add-flow $XDP_OVSBR table=0,dl_vlan=$VLAN,tcp,in_port=p1,actions=strip_vlan,mod_dl_dst:$MAC_SF,output:$SF_REP
+ovs-ofctl add-flow $XDP_OVSBR table=0,dl_vlan=$VLAN,tcp,in_port=${XDP_UPLINK},actions=strip_vlan,mod_dl_dst:$MAC_SF,output:$SF_REP
 
 #/* Send the packet from the NS to the RP’s public for the tenant */
 ovs-ofctl add-flow -O OpenFlow13 $XDP_OVSBR "table=0,in_port=$VETH_OVS_SIDE,actions=push_vlan:0x8100,set_field:${OF_VLAN}->vlan_vid,output:${BR_INT_PATCH}"
@@ -121,9 +130,23 @@ ovs-ofctl add-flow -O OpenFlow13 $XDP_OVSBR "table=0,in_port=$VETH_OVS_SIDE,acti
 ovs-ofctl add-flow $XDP_OVSBR table=0,dl_vlan=$VLAN,tcp,in_port=$BR_INT_PATCH,actions=strip_vlan,mod_dl_dst:$MAC_VETH_NS,output:$VETH_OVS_SIDE
 
 #/* Send the packet from the NS to the external client */
-ovs-ofctl add-flow -O OpenFlow13 brp1 "table=0,in_port=$SF_REP,actions=push_vlan:0x8100,set_field:$OF_VLAN->vlan_vid,output:p1"
+ovs-ofctl add-flow -O OpenFlow13 ${XDP_OVSBR} "table=0,in_port=$SF_REP,actions=push_vlan:0x8100,set_field:$OF_VLAN->vlan_vid,output:${XDP_UPLINK}"
 
 
+# Set up the XDP namespace
 
+ip netns exec $NS sysctl -w net.ipv4.conf.all.forwarding=1
+
+ip netns exec $NS iptables -t raw -I PREROUTING -i $SF -p tcp -m tcp --syn  -j CT --notrack
+ip netns exec $NS iptables -A FORWARD -i $SF -p tcp -m tcp   -m state --state INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 2 --mss 1460
+ip netns exec $NS iptables  -A FORWARD -m state --state INVALID -j DROP
+
+ip netns exec $NS /sbin/sysctl -w net/ipv4/tcp_timestamps=1
+ip netns exec $NS /sbin/sysctl -w net/netfilter/nf_conntrack_tcp_loose=0
+
+# ip netns exec $NS ip link set $SF xdp off
+# ip netns exec $NS ip link set $SF xdp object /opt/mellanox/xdp/lib64/xdp.o   section xdp/syncookie
+
+ip netns exec $NS  ethtool --set-priv-flags $SF tx_xdp_hw_checksum  on
 
 
